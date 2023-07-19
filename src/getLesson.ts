c319e2c424ca4489dd79cbac0e9b794f339d6da8
misha <dedovmu@gmail.com>
2023-07-19 19:08:29 +0300

    Working getLesson query

--- src/getLesson.ts
+++ src/getLesson.ts
@@ -1,82 +1,102 @@
-import { queryDb } from "./queryDb.js"
+import { QueryResult } from "pg";
+import type { QueryConfig } from "pg";
+
+import { queryDb } from "./queryDb.js";
 
 type Student = {
-    id: string,
-    name: string,
-    visit: boolean
-}
+  id: string;
+  name: string;
+  visit: boolean;
+};
 
 type Teacher = {
-    id: string,
-    name: string
-}
+  id: string;
+  name: string;
+};
 
 type Lesson = {
-    id: string,
-    date: Date,
-    title: string,
-    status: boolean,
-    visitCount: number,
-    students: Student[],
-    teachers: Teacher[],
-}
+  id: string;
+  date: Date;
+  title: string;
+  status: boolean;
+  visitCount: number;
+  students: Student[];
+  teachers: Teacher[];
+};
 
 type getLessonOptions = {
-    date?: [Date, Date] | Date,
-    title: String[],
-    status?: boolean,
-    teacherIds: String[],
-    studentCount?: [number, number] | number,
-    page: number,
-    lessonsPerPage: number
-}
+  date?: Date | [Date, Date];
+  status?: boolean;
+  teacherIds?: string[];
+  studentsCount?: number | [number, number];
+  page: number;
+  lessonsPerPage: number;
+};
 
 const defaultOptions: getLessonOptions = {
-    title: [],
-    teacherIds: [],
-    page: 1,
-    lessonsPerPage: 5
+  page: 1,
+  lessonsPerPage: 5,
+};
+
+export async function getLessons(
+  opts: getLessonOptions = defaultOptions
+): Promise<QueryResult<any> | void> {
+  // todo validate query
+  let config = buildGetQuery(opts);
+  let res = await queryDb(config);
+  return res;
 }
 
-export async function getLessons(opts: getLessonOptions = defaultOptions): Promise<Lesson[] | void> {
-    // todo validate query
-    let query = 'SELECT * FROM lessons;'//buildGetQuery(opts)
-    let res = await queryDb(query)
-    console.log(res.rows[0])
+function buildGetQuery(opts: getLessonOptions): QueryConfig {
+  // TODO calculate limit and offset
+  let limit, offset;
+  let i = 1;
+  //
+  //SELECT lessons.*, COUNT(lesson_students.visit) visited FROM lessons JOIN lesson_students ON lessons.id = lesson_students.lesson_id WHERE lesson_students.visit = 't' GROUP BY lessons.id;
+  //
+  //
+  let query = "SELECT lessons.*, COUNT(lesson_students.visit) visited FROM lessons JOIN lesson_students ON lessons.id = lesson_students.lesson_id WHERE lesson_students.visit = 't' GROUP BY lessons.id"
+
+  let params = [];
+
+  // TODO range
+  if (opts.date) {
+    query += ` date = $${i}`;
+    params.push(opts.date);
+    i++;
+  }
+  if (opts.status !== undefined) {
+    query += ` AND status = $${i}`;
+    params.push(opts.status ? "1" : "0");
+    i++;
+  }
+  if (opts.teacherIds) {
+    query += ` AND id IN (SELECT lesson_id FROM lesson_teachers WHERE teacher_id in ($${i})`;
+    params.push(opts.teacherIds);
+    i++;
+  }
+  if (opts.studentsCount) {
+    query += ` AND id IN (SELECT lesson_id FROM lesson_students GROUP BY lesson_id HAVING COUNT(DISTINCT(student_id)) > $${i})`;
+    params.push(opts.studentsCount);
+    i++;
+  }
+
+  query += ` JOIN lesson_students ON lesson_students.lesson_id = lessons.id`
+  query += ` JOIN lesson_teachers ON lesson_teachers.lesson_id = lessons.id`
+  query += ` LIMIT $${i}`;
+
+
+
+
+
+  params.push(opts.lessonsPerPage);
+  i++;
+  if (offset) {
+    query += ` OFFSET $${i}`;
+    params.push(opts.page * opts.lessonsPerPage);
+  }
+
+  return { text: query, values: params };
 }
 
-function buildGetQuery(opts: getLessonOptions): string {
-    // TODO calculate limit and offset
-    let limit, offset
-    let and = false
-    let i = 1
-    let query = 'SELECT * FROM lessons'
-    
-    // TODO range
-    if (opts.date) {
-        query.concat(` WHERE date = $${i}`)
-        i++
-        and = true
-    }
-    if (opts.status) {
-        and ? query.concat(' AND') : query.concat(' WHERE')
-        query.concat(` status = $${i}`)
-        i++
-        and = true
-    }
-    if (opts.teacherIds) {
-        and ? query.concat(' AND') : query.concat(' WHERE')
-        query.concat(`WHERE id IN (SELECT lesson_id FROM lesson_teachers WHERE teacher_id in ($${i})`)
-        i++
-        and = true
-    }
-    if (opts.studentCount) {
-        and ? query.concat(' AND') : query.concat(' WHERE')
-        query.concat(`WHERE id IN (SELECT lesson_id FROM lesson_students GROUP BY lesson_id HAVING COUNT(DISTINCT(student_id)) > $${i})`)
-    }
-    query.concat(` LIMIT = $${i}`)
-    i++
-    offset && query.concat(` OFFSET = $${i}`)
-
-    return query
-}
+getLessons()
--- src/queryDb.ts
+++ src/queryDb.ts
@@ -1,15 +1,17 @@
 import pg from 'pg'
-const { Client } = pg;
+const { Client } = pg
+
 
 const cfg = {
     database: 'moyklass-test',
     user: 'dimon'
 }
 
-export async function queryDb(query: string) {
+// TODO remove any
+export async function queryDb(config: pg.QueryConfig<any>) {
     const client = new Client(cfg)
     await client.connect()
-    const res = await client.query(query)
+    const res = await client.query(config)
     await client.end()
     //access data: res.rows[0]
     return res
